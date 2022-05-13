a19d6c7611e33ea80d72b2d64379e476f61cdf38
Xlazzi <97210032+Xlazzi@users.noreply.github.com>
2022-05-13 07:52:52 +0700

    Update

---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div className='CSBH'>Chính sách bảo hành</div>
+        <div className='LH'>liên hệ</div> 
+    </div>
+  )
+}
---
+++ src/head.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='Head'>
+        <div className='header'>
+        <div className='logo'>Tiki</div>
+        <button>TimKiem</button>
+   </div>
+    </div>
+  )
+}
